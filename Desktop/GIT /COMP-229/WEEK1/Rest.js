e72168905db597cb383976079dadbc9f9028a4c1
Achint Rekhi <achintsingh2503@gmail.com>
2023-09-11 12:04:56 -0400

    first commit

---
+++ Desktop/GIT /COMP-229/WEEK1/Rest.js
@@ -0,0 +1,4 @@
+function userFriends(user, ...friends) {
+   console.log(user + ' has ' + friends.length + ' friends and they are ' + friends[0] + ' and ' + friends[1]);
+}
+userFriends('Achint', 'Bob', 'Alice');
